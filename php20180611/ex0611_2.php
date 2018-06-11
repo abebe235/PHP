6c4e0338ebd5b8a05a574ac0a361bbe91f2a3a74
F.abebe <musami633@gmail.com>
2018-06-11 13:11:08 +0900

    前回の続き〜ユーザーにメール送信（途中）

--- php20180611/ex0611_2.php
+++ php20180611/ex0611_2.php
@@ -12,14 +12,20 @@ mb_language("Japanese") ;
 mb_internal_encoding("UTF-8") ; //このフォーマットに従って記号化され送信
 
 $to = 'musami633@gmail.com' ;
-$title = 'テストメール' ;
+$title = '受付致しました。' ;
 $content = htmlspecialchars($_POST[email]) ;
 $header = "From: $from_email\nReplay-To: $from_email\n" ;
 
 if(mb_send_mail($to, $title, $content, $header)){
-    echo 'メールを送信しました。' ;
+    echo 'メールを送信しました。ご利用ありがとうございます。' ;
 } else {
     echo 'メールの送信に失敗しました。' ;
 } ;
-　
+
+/*
+$_POST[userdate] = '姓名,ふりがな,〒,郵便番号,住所,電話,コメント,メールアドレス'
+$sepdate = explode(',',$_POST[userdate]) ;
+$explode = 
+*/
+
 ?>
